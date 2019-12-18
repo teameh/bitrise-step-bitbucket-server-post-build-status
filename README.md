333cb502868ec24abb363fb733434ded50e85744
Tieme van Veen <tiemevanveen@hotmail.com>
2019-12-18 14:48:52 +0100

    build_status can only be 0 or 1; improve logging

--- README.md
+++ README.md
@@ -3,3 +3,29 @@
 Post build status to bitbucket server
 
 See step.yml for inputs
+
+### Basic example
+
+Typically you want to notify bitbucket when a build is started and when a build has finished. You can do this by adding this step twice to the steps of your workflow:
+
+```
+  my-workflow:
+    steps:
+    
+    - bitbucket-server-post-build-status:
+        inputs:
+        - bitbucket_server_username: "$BITBUCKET_SERVER_USERNAME"
+        - bitbucket_server_password: "$BITBUCKET_SERVER_PASSWORD"
+        - bitbucket_server_domain: "$BITBUCKET_SERVER_DOMAIN"
+        - build_is_in_progress: true
+    
+    - other-steps..
+    - ..for-this-workflow
+    
+    - bitbucket-server-post-build-status:
+        inputs:
+        - bitbucket_server_username: "$BITBUCKET_SERVER_USERNAME"
+        - bitbucket_server_password: "$BITBUCKET_SERVER_PASSWORD"
+        - bitbucket_server_domain: "$BITBUCKET_SERVER_DOMAIN"
+
+```
--- step.sh
+++ step.sh
@@ -1,19 +1,84 @@
 #!/usr/bin/env bash
 
-case $build_status in
-0) BITBUCKET_BUILD_STATE="INPROGRESS" ;; # Not finished
-1) BITBUCKET_BUILD_STATE="SUCCESSFUL" ;; # Successful
-2) BITBUCKET_BUILD_STATE="FAILED"     ;; # Failed
-3) BITBUCKET_BUILD_STATE="FAILED"     ;; # Aborted with failure
-4) BITBUCKET_BUILD_STATE="SUCCESSFUL" ;; # Aborted with success
-esac
+join_ws() { local IFS=; local s="${*/#/$1}"; echo "${s#"$1$1$1"}"; }
+joinStrings() { local a=("${@:3}"); printf "%s" "$2${a[@]/#/$1}"; }
+
+INVALID_INPUT=false
+if [ -z "$bitbucket_server_domain" ]; then
+  INVALID_INPUT=true
+  echo "- Missing input field: bitbucket_server_domain"
+fi
+
+if [ -z "$bitbucket_server_username" ]; then
+  INVALID_INPUT=true
+  echo "- Missing input field: bitbucket_server_username"
+fi
+
+if [ -z "$bitbucket_server_password" ]; then
+  INVALID_INPUT=true
+  echo "- Missing input field: bitbucket_server_password"
+fi
+
+if [ -z "$git_clone_commit_hash" ]; then
+  INVALID_INPUT=true
+  echo "- Missing input field: git_clone_commit_hash"
+fi
+
+if [ -z "$app_title" ]; then
+  INVALID_INPUT=true
+  echo "- Missing input field: app_title"
+fi
+
+if [ -z "$build_number" ]; then
+  INVALID_INPUT=true
+  echo "- Missing input field: build_number"
+fi
+
+if [ -z "$build_url" ]; then
+  INVALID_INPUT=true
+  echo "- Missing input field: build_url"
+fi
+
+if [ -z "$triggered_workflow_id" ]; then
+  INVALID_INPUT=true
+  echo "- Missing input field: triggered_workflow_id"
+fi
+
+if [ -z "$build_status" ] && [ -z "$build_is_in_progress" ]; then
+  INVALID_INPUT=true
+  echo "- Missing input field: build_status OR build_status"
+fi
+
+if [ ! -z $build_status ]; then
+  if [ "$build_status" == 0 ]; then
+    BITBUCKET_BUILD_STATE="SUCCESSFUL"
+  elif [ "$build_status" == 1 ]; then
+    BITBUCKET_BUILD_STATE="FAILED"
+  else
+    echo "- Invalid build_status: ${build_status}";
+    INVALID_INPUT=true
+  fi
+fi
+
+if [ ! -z "$build_is_in_progress" ] && [ "$build_is_in_progress" == true ]; then
+  BITBUCKET_BUILD_STATE="INPROGRESS"
+fi
+
+if [ -z "$BITBUCKET_BUILD_STATE" ]; then
+  echo "- build_status is not set and build_is_in_progress is unset or not true";
+  INVALID_INPUT=true
+fi
+
+if [ "$INVALID_INPUT" == true ]; then
+  exit 1
+fi
 
 BITBUCKET_API_ENDPOINT="https://$bitbucket_server_domain/rest/build-status/1.0/commits/$git_clone_commit_hash"
 
 echo "Post build status: $BITBUCKET_BUILD_STATE"
 echo "API Endpoint: $BITBUCKET_API_ENDPOINT"
 
-curl https://$bitbucket_server_domain/rest/build-status/1.0/commits/$git_clone_commit_hash \
+curl $BITBUCKET_API_ENDPOINT \
   -X POST \
   -i \
   -u $bitbucket_server_username:$bitbucket_server_password \
